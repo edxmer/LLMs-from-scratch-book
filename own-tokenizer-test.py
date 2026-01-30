2433ba6dfdabd1d528a458598d0a03b7f3e05fb3
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-01-30 14:08:31 +0100

    added comments and explanations

--- own-tokenizer-test.py
+++ own-tokenizer-test.py
@@ -1,12 +1,17 @@
 import re
 
+# I am doing word tokenizing, not byte-pair encoding
+
+# open the training data
 with open("the_verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-all_tokens = re.split("([.,:;?_!'\"()!]|--|\\s)", raw_text)
-all_tokens = [item.strip() for item in all_tokens if item.strip()]
-all_tokens = sorted(set(all_tokens))
-all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+# creating the tokens
+all_tokens = re.split("([.,:;?_!'\"()!]|--|\\s)", raw_text) # make an array that contains all words without symbols, all symbols, and all spaces
+all_tokens = [item.strip() for item in all_tokens if item.strip()] # strip all of space characters, and remove space characters
+all_tokens = sorted(set(all_tokens)) # put them into a set so everything will be unique, then sort them
+all_tokens.extend(["<|endoftext|>", "<|unk|>"]) # add special tokens to the end
 
 
 class SimpleTokenizer:
@@ -30,8 +35,8 @@ t2 = "In the sunlit terraces of the palace."
 ts = " <|endoftext|> ".join([t1, t2])
 
 ids = tokenizer.text_to_ids(ts)
-print(ids[:50])
+print(ids)
 words = tokenizer.ids_to_words(ids)
-print(words[:400])
+print(words) # Because hello and tea wasn't in the text, it will be treated as <|unk|>.
 
 
--- tiktoken-test-1.py
+++ tiktoken-test-1.py
@@ -1,15 +1,22 @@
 import tiktoken
+# Tiktoken uses byte-pair encoding, so it can handle most unknown words and symbols.
+# Byte pair encoding works by first including all simple characters and symbols, adding combinations for the most common ones based on the training data
+
 
 with open("the_verdict.txt", "r") as f:
     raw_text = f.read()
 
+# the gpt2 encoder has 50257 tokens
 tokenizer = tiktoken.get_encoding("gpt2")
 
+
 t = "Akwirw ier"
+
+
 
 integers = tokenizer.encode(t)
 
 print(integers, tokenizer.decode(integers))
 
 for i in integers:
-    print(i, tokenizer.decode([i]))
+    print(i, "-", tokenizer.decode([i]))
